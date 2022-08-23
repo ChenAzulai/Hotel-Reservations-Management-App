8b2841e5e3b998274daa30e578a1c17ea5876403
ChenAzulai <ChenAzulai@users.noreply.github.com>
2022-08-23 14:45:07 +0300

    fix: practical changes and bugs

--- force-app/main/default/lwc/addNewHotel/addNewHotel.js
+++ force-app/main/default/lwc/addNewHotel/addNewHotel.js
@@ -5,14 +5,9 @@ import BILLING_ADDRESS_FIELD from "@salesforce/schema/Account.BillingAddress";
 import PICTURE from "@salesforce/schema/Account.Picture__c";
 
 export default class AddNewHotel extends LightningElement {
-  fields = [
-    // NAME_FIELD,
-    { label: "Hotel Name", fieldName: NAME_FIELD.fieldApiName, type: "text" },
-    BILLING_ADDRESS_FIELD,
-    PICTURE
-  ];
+  fields = [NAME_FIELD, BILLING_ADDRESS_FIELD, PICTURE];
   objectApiName = "Account";
-
+  recordTypeId = "Hotel";
   handleSuccess(event) {
     const toastEvent = new ShowToastEvent({
       title: "Hotel has been created successfully!",
--- force-app/main/default/lwc/hotelsInGoogleMap/hotelsInGoogleMap.js
+++ force-app/main/default/lwc/hotelsInGoogleMap/hotelsInGoogleMap.js
@@ -1,22 +1,42 @@
-import { LightningElement, wire } from "lwc";
+import { LightningElement, wire, track } from "lwc";
 import getAccounts from "@salesforce/apex/AccountController.getAccounts";
-const ZOOM_LEVEL = 10;
+const ZOOM_LEVEL = 2;
 
 export default class HotelsInGoogleMap extends LightningElement {
-  appMarkers = [];
-  error;
-  zoomLevel = ZOOM_LEVEL;
+  @track mapMarkers = [];
+  @track error;
+  @track zoomLevel = ZOOM_LEVEL;
+  @track mapVisible = false;
   listView = "visible";
 
   @wire(getAccounts)
   wiredAccounts({ error, data }) {
     if (data) {
-      this.appMarkers = data;
-      console.log("data:", data);
+      console.log(data);
+      //   JSON.stringify(data);
+      data.forEach((hotel) => {
+        if (!hotel.BillingAddress) return;
+        const marker = {
+          location: {
+            City: hotel.BillingAddress?.city,
+            Country: hotel.BillingAddress?.country,
+            PostalCode: hotel.BillingAddress?.postalCode,
+            State: hotel.BillingAddress?.state,
+            Street: hotel.BillingAddress?.street
+          },
+          title: hotel.Name,
+          description: hotel.description ? hotel.description : "Hotel",
+          icon: "standard:account"
+        };
+        const { location } = marker;
+        if (location.Country || location.PostalCode)
+          this.mapMarkers.push(marker);
+      });
+      this.mapVisible = true;
       this.error = undefined;
     } else if (error) {
       this.error = error;
-      this.appMarkers = undefined;
+      this.mapMarkers = undefined;
     }
   }
 }
