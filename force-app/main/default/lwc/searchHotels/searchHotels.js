2887ed98e733854de2ea81dbc92cf8d541ab9d61
ChenAzulai <ChenAzulai@users.noreply.github.com>
2022-08-23 21:14:10 +0300

    feat: data table with image

--- force-app/main/default/lwc/searchHotels/searchHotels.js
+++ force-app/main/default/lwc/searchHotels/searchHotels.js
@@ -1,3 +1,33 @@
-import { LightningElement } from "lwc";
+import { LightningElement, track } from "lwc";
+import getSearchedHotels from "@salesforce/apex/accountController.getSearchedHotels";
+export default class SearchHotels extends LightningElement {
+  key;
+  @track results;
+  @track error;
+  updateKey(event) {
+    this.key = event.target.value;
+  }
 
-export default class SearchHotels extends LightningElement {}
+  handleSearch() {
+    getSearchedHotels({ searchKey: this.key })
+      .then((result) => {
+        console.log("result:", result);
+        this.results = result;
+        this.error = null;
+      })
+      .catch((error) => {
+        this.error = error;
+        this.results = null;
+      });
+  }
+  cols = [
+    { label: "Hotel name", fieldName: "Name", type: "text" },
+    {
+      label: "Hotel picture",
+      type: "customImage",
+      typeAttributes: {
+        hotelImage: { fieldName: "Picture__c" }
+      }
+    }
+  ];
+}
